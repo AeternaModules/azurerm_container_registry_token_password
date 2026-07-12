180377e2520d95ce8e168be5055ca13ff8a17a2d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,15 @@
-output "container_registry_token_passwords" {
-  description = "All container_registry_token_password resources"
-  value       = azurerm_container_registry_token_password.container_registry_token_passwords
-  sensitive   = true
-}
 output "container_registry_token_passwords_container_registry_token_id" {
-  description = "List of container_registry_token_id values across all container_registry_token_passwords"
-  value       = [for k, v in azurerm_container_registry_token_password.container_registry_token_passwords : v.container_registry_token_id]
+  description = "Map of container_registry_token_id values across all container_registry_token_passwords, keyed the same as var.container_registry_token_passwords"
+  value       = { for k, v in azurerm_container_registry_token_password.container_registry_token_passwords : k => v.container_registry_token_id }
 }
 output "container_registry_token_passwords_password1" {
-  description = "List of password1 values across all container_registry_token_passwords"
-  value       = [for k, v in azurerm_container_registry_token_password.container_registry_token_passwords : v.password1]
+  description = "Map of password1 values across all container_registry_token_passwords, keyed the same as var.container_registry_token_passwords"
+  value       = { for k, v in azurerm_container_registry_token_password.container_registry_token_passwords : k => v.password1 }
   sensitive   = true
 }
 output "container_registry_token_passwords_password2" {
-  description = "List of password2 values across all container_registry_token_passwords"
-  value       = [for k, v in azurerm_container_registry_token_password.container_registry_token_passwords : v.password2]
+  description = "Map of password2 values across all container_registry_token_passwords, keyed the same as var.container_registry_token_passwords"
+  value       = { for k, v in azurerm_container_registry_token_password.container_registry_token_passwords : k => v.password2 }
   sensitive   = true
 }
 
