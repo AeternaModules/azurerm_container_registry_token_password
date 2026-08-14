34cce3e7f78790d288a0abc79de9ecc12432ac9d
Daniel Allen <dan@aeternagroup.net>
2026-08-15 00:51:44 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,12 @@ output "container_registry_token_passwords_container_registry_token_id" {
 }
 output "container_registry_token_passwords_password1" {
   description = "Map of password1 values across all container_registry_token_passwords, keyed the same as var.container_registry_token_passwords"
-  value       = { for k, v in azurerm_container_registry_token_password.container_registry_token_passwords : k => v.password1 if v.password1 != null && length(v.password1) > 0 }
+  value       = { for k, v in azurerm_container_registry_token_password.container_registry_token_passwords : k => one(v.password1) if v.password1 != null && length(v.password1) > 0 }
   sensitive   = true
 }
 output "container_registry_token_passwords_password2" {
   description = "Map of password2 values across all container_registry_token_passwords, keyed the same as var.container_registry_token_passwords"
-  value       = { for k, v in azurerm_container_registry_token_password.container_registry_token_passwords : k => v.password2 if v.password2 != null && length(v.password2) > 0 }
+  value       = { for k, v in azurerm_container_registry_token_password.container_registry_token_passwords : k => one(v.password2) if v.password2 != null && length(v.password2) > 0 }
   sensitive   = true
 }
 
